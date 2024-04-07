31b3b6b1b9cd92f6318c5f1f1ab9942257666760
Tetiana <tetiana_reshetnik@epam.com>
2024-04-07 22:23:41 +0300

    fix file parsing

--- import-service/handlers/importFileParser.mjs
+++ import-service/handlers/importFileParser.mjs
@@ -5,63 +5,71 @@ import {
   DeleteObjectCommand,
 } from "@aws-sdk/client-s3";
 import csvParser from "csv-parser";
-import stream from "stream";
 import { promisify } from "util";
+import { pipeline, Writable } from "stream";
 
 const s3Client = new S3Client({ region: "eu-west-1" });
-const pipeline = promisify(stream.pipeline);
+const pipelineAsync = promisify(pipeline);
 
-async function moveFile(bucketName, sourceKey, destinationKey) {
+async function moveFile(bucketName, objectKey, destinationKey) {
   await s3Client.send(
     new CopyObjectCommand({
       Bucket: bucketName,
-      CopySource: `${bucketName}/${sourceKey}`,
+      CopySource: `${bucketName}/${objectKey}`,
       Key: destinationKey,
     })
   );
   await s3Client.send(
     new DeleteObjectCommand({
       Bucket: bucketName,
-      Key: sourceKey,
+      Key: objectKey,
     })
   );
 }
 
+const processCsv = async (bucketName, objectKey) => {
+  const getObjectParams = {
+    Bucket: bucketName,
+    Key: objectKey,
+  };
+
+  try {
+    const data = await s3Client.send(new GetObjectCommand(getObjectParams));
+
+    await await pipelineAsync(
+      data.Body,
+      csvParser(),
+      new Writable({
+        objectMode: true,
+        write(chunk, encoding, callback) {
+          console.log(chunk);
+          callback();
+        },
+      })
+    );
+
+    console.log(`Successfully processed ${objectKey}`);
+  } catch (err) {
+    console.error("Error processing file", err);
+    throw err;
+  }
+};
+
 export const importFileParser = async (event) => {
   for (const record of event.Records) {
     const bucketName = record.s3.bucket.name;
-    const sourceKey = decodeURIComponent(
+    const objectKey = decodeURIComponent(
       record.s3.object.key.replace(/\+/g, " ")
     );
 
     try {
-      const getObjectParams = {
-        Bucket: bucketName,
-        Key: sourceKey,
-      };
+      await processCsv(bucketName, objectKey);
 
-      const readStream = s3Client
-        .send(new GetObjectCommand(getObjectParams))
-        .createReadStream();
+      console.log(`CSV file ${objectKey} has been processed`);
 
-      // Parsing the CSV file
-      await pipeline(
-        readStream,
-        csvParser(),
-        new stream.Writable({
-          objectMode: true,
-          write(record, encoding, callback) {
-            console.log(record);
-            callback();
-          },
-        })
-      );
-
-      console.log(`CSV file ${sourceKey} has been processed`);
-
-      const destinationKey = sourceKey.replace("uploaded/", "parsed/");
-      await moveFile(bucketName, sourceKey, destinationKey);
-      console.log(`File moved from ${sourceKey} to ${destinationKey}`);
+      const destinationKey = objectKey.replace("uploaded/", "parsed/");
+      await moveFile(bucketName, objectKey, destinationKey);
+      console.log(`File moved from ${objectKey} to ${destinationKey}`);
     } catch (error) {
       console.error("Error processing/moving file:", error);
       throw error;
